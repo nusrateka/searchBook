a69a74e60534d227f8f87408c9819e807690f3b2
Nusrat Jahan Eka <nusrat.jahaneka@unipro.co.jp>
2020-02-25 12:30:22 +0900

    Updates

---
+++ precache-manifest.311104bb924907dcc58f4f04192817c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3049ed2f465b8072ab96183cf04cac",
+    "url": "/searchBook/index.html"
+  },
+  {
+    "revision": "b630bf9105ecbad7a41c",
+    "url": "/searchBook/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d9929382f14a71645402",
+    "url": "/searchBook/static/js/2.429b2cf8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/searchBook/static/js/2.429b2cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b630bf9105ecbad7a41c",
+    "url": "/searchBook/static/js/main.5b74ba78.chunk.js"
+  },
+  {
+    "revision": "6dcb0c45eaac0b08e917",
+    "url": "/searchBook/static/js/runtime-main.6be8f8d2.js"
+  }
+]);
